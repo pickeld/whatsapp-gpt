35cc1aa27aee0a8059f96c46a1279974b29ab1bb
David Pickel <pickeld@gmail.com>
2025-07-01 14:11:50 +0300

    Enhance GPT class: Add system prompt to chat messages for improved context

--- providers/gpt.py
+++ providers/gpt.py
@@ -1,8 +1,10 @@
 from config import config
 from openai import OpenAI
 from utiles.logger import Logger
+from providers.prompts import build_system_prompt
 
 logger = Logger(__name__)
+
 
 class GPT:
     def __init__(self):
@@ -11,10 +13,13 @@ class GPT:
         self.client = OpenAI(api_key=config.openai_api_key)
 
     def chat(self, prompt: str):
-        logger.info(f"Sending prompt to OpenAI: {prompt}")
         response = self.client.chat.completions.create(
             model=self.openai_model,
-            messages=[{"role": "user", "content": prompt}]
+            messages=[
+                {"role": "system", "content": build_system_prompt()},
+                {"role": "user", "content": prompt}
+                ],
+            temperature=float(config.openai_temperature),
         )
         reply = response.choices[0].message.content
         return reply
