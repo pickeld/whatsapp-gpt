c1a9d0ca52ad4c93ed58d96d264c41ce55f2725d
David Pickel <pickeld@gmail.com>
2025-07-05 16:05:05 +0300

    Refactor MemoryAgent: Simplify agent management and enhance message handling

--- memory_agent.py
+++ memory_agent.py
@@ -1,61 +1,72 @@
-from letta_client import EmbeddingConfig, Letta, LlmConfig, MessageCreate
+from letta_client import AgentState, EmbeddingConfig, Letta, MessageCreate, MessageCreateContent, TextContent, ImageContent
+from letta_client.types.agent_state import AgentState
+from typing import Union
+from letta_client import Base64Image, ImageContent, TextContent
 
+
+
+SUPPORTED_MEDIA_TYPES = {"image/jpeg", "image/png"}
 
 class MemoryAgent:
-    def __init__(self, base_url: str ="http://localhost:8283"):
-        self.client = Letta(base_url=base_url)
+    def __init__(self, recipient: str):
+        self.llm_model_name = "gpt-4.1-mini"
+        self.recipient = recipient
+        self.chat_id = recipient.replace("@", "_").replace(".", "_")
+        self.client = Letta(base_url="http://localhost:8283")
+        self.agent: AgentState = self.get_agent()
+        
     
-    def get_models(self, name="gpt-4.1-mini"):
+    def get_models(self):
         models = self.client.models.list()
-        if not models:
-            raise ValueError(f"No models found with name: {name}")
         for model in models:
-            if model.model == name:
+            if model.model == self.llm_model_name:
                 return model
+        raise ValueError(f"Model {self.llm_model_name} not found in available models.")
     
-    def get_agent(self, chat_id: str):
-        agents = self.client.agents.list(name=chat_id)
-        if agents:
-            agent = agents[0]
-        else:
-            agent =  self.set_agent(chat_id)
-        return agent
+    def get_agent(self) -> AgentState:
+        agents = self.client.agents.list(name=self.chat_id)
+        return agents[0] if agents else self.set_agent()
     
-    def set_agent(self, chat_id: str):
-        agent = self.client.agents.create(
-            name=chat_id,
+    def set_agent(self) -> AgentState:
+        return self.client.agents.create(
+            name=self.chat_id,
             llm_config=self.get_models(),
-            embedding_config=EmbeddingConfig(embedding_endpoint_type="openai",
-                                             embedding_model="text-embedding-3-small",
-                                             embedding_dim=1024,)
+            embedding_config=EmbeddingConfig(
+                embedding_endpoint_type="openai",
+                embedding_model="text-embedding-3-small",
+                embedding_dim=1024,
             )
-        return agent
+        )
         
-    def send_message(self, payload: dict):
-        chat_id = payload.get("to", "DEFAULT_CHAT_ID")
-        normalized_chat_id:str = chat_id.replace("@", "_").replace(".", "_")
-        message = payload.get("body", "").strip()
+    def send_message(self, whatsapp_msg):
+        content: list[Union[TextContent, ImageContent]] = []
+        
+        if whatsapp_msg.has_media and whatsapp_msg.media.type in SUPPORTED_MEDIA_TYPES:
+            content.append(ImageContent(
+                source=Base64Image(
+                    type= "base64",
+                    media_type= whatsapp_msg.media.type,
+                    data= whatsapp_msg.media.base64
+                )
+            ))
+        if whatsapp_msg.quoted:
+            if whatsapp_msg.quoted.type == "chat":
+                content.append(TextContent(text=f"{whatsapp_msg.message}\n\n(Quoted): {whatsapp_msg.quoted.body}"))
+            else:
+                content.append(TextContent(text=f"{whatsapp_msg.message}"))
+            if whatsapp_msg.quoted.type == "image" and whatsapp_msg.quoted.mimetype in SUPPORTED_MEDIA_TYPES:
+                content.append(ImageContent(
+                    source=Base64Image(
+                        type= "base64",
+                        media_type= whatsapp_msg.quoted.mimetype,
+                        data= whatsapp_msg.quoted.base64_data)
+                ))
+                
         response = self.client.agents.messages.create(
-            agent_id=self.get_agent(normalized_chat_id).id,
-            messages=[
-            MessageCreate(role="user",
-                          content=message
-                          )
-            ]
+            agent_id=self.agent.id,
+            messages=[MessageCreate(role="user", content=content)]
         )
         
-        assistant_reply = next(
-            m for m in response.messages if m.message_type == "assistant_message"
-        )
-        print(f"Assistant Reply: {assistant_reply.content}")
+        assistant_reply = next(m for m in response.messages if m.message_type == "assistant_message")
+        # print(f"Assistant Reply: {assistant_reply.content}")
         return assistant_reply.content
-        
-        
-        
-
-if __name__ == "__main__":
-    memory_agent = MemoryAgent()
-    agent = memory_agent.get_agent("test_agent1211111")    
-    memory_agent.send_message(agent_id=agent.id,
-                              message="Hello, this is a test message."
-                              )
